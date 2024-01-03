d9a2893b31804e4446ae4295f569e8349d91c3fa
Jean-Baptiste Bouillat <jbb@macbook-pro-de-jean-baptiste.home>
2024-01-03 16:03:42 +0100

    update sign out position

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -4,8 +4,12 @@ type Props = {}
 
 const Profile = (props: Props) => {
   return (
-    <div>Profile</div>
-  )
+    <section>
+      <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-10">
+        <h2 className="text-center text-3xl font-bold md:text-5xl">Profile</h2>
+        <p className="mx-auto mb-8 mt-4 text-center text-sm text-[#636262] sm:text-base md:mb-12 lg:mb-16">Feel free to change your credentials at any time.</p>
+      </div>
+    </section>  )
 }
 
 export default Profile
--- src/components/shared/Navigation/SideBar.tsx
+++ src/components/shared/Navigation/SideBar.tsx
@@ -33,11 +33,6 @@ const menuItems: IMenuItem[] = [
         path: ERoutes.USER_PROFILE,
         icon: <ProfileIcon width={ICON_WIDTH}/>
     },
-    {
-        title: "Sign Out",
-        path: ERoutes.LOGIN,
-        icon: <LogoutIcon width={ICON_WIDTH}/>
-    },
 ] 
 
 const SideBar = observer(() => {
@@ -62,6 +57,10 @@ const SideBar = observer(() => {
         navigate(route);
         toggleSidebar();
     };
+
+    const signout = () => {
+        navigate(ERoutes.LOGIN);
+    }
 
     const isActive = (item: IMenuItem):boolean => {
         if (item.path === location.pathname || location.pathname.includes(item.path)) 
@@ -96,17 +95,20 @@ const SideBar = observer(() => {
                                 </button>
                                 {isUserMenuOpen && (
                                     <div className="z-50 absolute top-14 right-0 mt-2 w-48 py-1 bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
-                                        <div className="px-4 py-3" role="none">
-                                            {/* <p className="text-sm text-gray-900 dark:text-white" role="none">
+                                        {/* <div className="px-4 py-3" role="none">
+                                            <p className="text-sm text-gray-900 dark:text-white" role="none">
                                                 Neil Sims
-                                            </p> */}
+                                            </p>
                                             <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
                                                 {user.user?.email}
                                             </p>
-                                        </div>
+                                        </div> */}
                                         <ul className="py-1" role="none">
-                                            <li>
-                                                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover.bg-gray-100 dark:text-gray-300 dark:hover.bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                                            <li className='flex cursor-pointer px-4' onClick={signout}>
+                                                <LogoutIcon width={ICON_WIDTH}/>
+                                                <span className="block px-4 py-2 text-sm text-gray-700 hover.bg-gray-100 dark:text-gray-300 dark:hover.bg-gray-600 dark:hover:text-white">
+                                                    Sign out
+                                                </span>
                                             </li>
                                         </ul>
                                     </div>
